16c08e9c6c6ec3f8e83c298f3685b7d2b3bc0dd3
Abdul Hajiyev <abdulhajiyev@gmail.com>
2025-05-12 14:14:00 +0400

    Refactor site URL configuration to simplify production check

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,12 +4,7 @@ import starlightThemeNova from 'starlight-theme-nova';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const isProduction = process.env.NODE_ENV === 'production';
-const site = isProduction
-  ? // @ts-ignore
-    process.env.SITE_URL
-  : 'http://localhost:4321/';
+const site = process.env.SITE_URL || 'http://localhost:4321/';
 
 // https://astro.build/config
 export default defineConfig({
